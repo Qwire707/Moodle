4d60ad965410e401f19ef6307dda290a386a0205
Qwire707 <danulovitryak299@gmail.com>
2026-02-11 20:26:42 +0200

    add models and views

--- assignments/urls.py
+++ assignments/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('assignment_list/', views.assignment_list_view, name='assignment_list'),
-    path('assignment_detail/', views.assignment_detail_view, name='assignment_detail'),
+    path('list/', views.assignment_list_view, name='assignment_list'),
+    path('detail/<int:pk>', views.assignment_detail_view, name='assignment_detail'),
     path('submission_list', views.submission_list_view, name='submission_list'),
-    path('grade_submission', views.grade_submission_view, name='grade_submission')
+    path('grade_submission/<int:pk>', views.grade_submission_view, name='grade_submission')
 ]
--- courses/urls.py
+++ courses/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('my/', views.my_courses_view, name='my_courses'),
     path('all/', views.all_courses_view, name='all_courses'),
     path('course/', views.course_detail_view, name='course_detail'),
-    path('enroll/', views.enroll_course_view, name='enroll_course'),
+    path('enroll/<int:pk>', views.enroll_course_view, name='enroll_course'),
     path('completed/', views.completed_course_view, name='completed_course'),
 ]
