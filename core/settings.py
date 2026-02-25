9b0dbecfbd2e5f7a7f39c454d0048ec63760b701
Qwire707 <danulovitryak299@gmail.com>
2026-02-25 19:52:49 +0200

    comp to deploy

--- core/settings.py
+++ core/settings.py
@@ -137,6 +137,6 @@ STATICFILES_STORAGE = 'whitenoise.storage. CompressedManifestStaticFilesStorage'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-ALLOWED_HOSTS = ['.onrender.com']
+ALLOWED_HOSTS = []
 
 
